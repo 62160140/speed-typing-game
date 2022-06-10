d9cb05120e07416e0570781d74c213cb35e9ff18
Pakawat Charoenying <62160140@go.buu.ac.th>
2022-06-10 16:12:45 +0700

    mode

--- script.js
+++ script.js
@@ -13,9 +13,11 @@ const overBtnEl = document.getElementById('over-btn')
 
 const words= ["monitor", "program", "application", "keyboard", "javascript", "gaming", "network"];
 
+let saveMode;
 let randomText;
 let score = 0;
 let time = 10; //easy=>15 , medium=>10 , hard="5"
+let level = 'medium';
 
 //ใช้งาน function updateTime ทุกๆ1วืนาที
 const timeInterval = setInterval(updateTime,1000)
@@ -25,21 +27,49 @@ function getRandomWord(){
 }
 
 function displayWordToUI(){
+  switch(saveMode){
+    case 'easy' :
+        time=15;
+        break;
+    case 'medium' :
+        time=10;
+        break;
+    case 'hard' :
+        time=5;
+        break;
+  }
+
   timeEl.innerHTML=`เวลา ${time} วินาที`
   randomText = getRandomWord();
   wordEl.innerHTML = randomText
+
+  
 }
 
 
 textEl.addEventListener('input',e=>{
   const inputText = e.target.value;
   if(inputText  === randomText.trim()){
+    increseTime()
     displayWordToUI();
     updateScore();
     e.target.value = '';
-    time+=5;
   }
 })
+
+function increseTime() {
+  switch (saveMode) {
+    case 'easy':
+      time += 5
+      break
+    case 'medium':
+      time += 3
+      break
+    case 'hard':
+      time += 2
+      break
+  }
+}
 
 function updateScore(){
   score+=10;
@@ -71,5 +101,26 @@ function gameOver(){
 
 }
 
-displayWordToUI()
+
+
+function startGame(){
+  saveMode = localStorage.getItem('mode') !== null ? localStorage.getItem('mode') : 'medium'
+  levelEl.value = saveMode 
+  displayWordToUI();
+}
+
+btnLevelEl.addEventListener('click',()=>{
+   settingsEl.classList.toggle('hide')
+})
+
+
+
+levelEl.addEventListener('change',e=>{
+  level = e.target.value;
+  localStorage.setItem('mode',level)
+  const saveMode = localStorage.getItem('mode') !== null ? localStorage.getItem('mode') : 'medium'
+  levelEl.value = saveMode 
+})
+
+startGame();
 textEl.focus();
